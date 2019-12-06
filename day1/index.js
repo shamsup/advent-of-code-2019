44d52dfc2a9a38afff9d79906e55be579575cc4a
Alex Mitchell <skid.biker.7@gmail.com>
2019-12-06 02:49:50 -0600

    day 1 part 2

--- day1/index.js
+++ day1/index.js
@@ -12,10 +12,16 @@ const roundDown = n => Math.floor(n);
 const minusTwo = n => n - 2;
 const sum = (a, b) => a + b;
 
-console.log(
-  numbers
-    .map(divideByThree)
-    .map(roundDown)
-    .map(minusTwo)
-    .reduce(sum)
-);
+const calculateFuelForMass = n => minusTwo(roundDown(divideByThree(n)));
+
+const calculateMassIncludingFuel = n => {
+  let totalFuel = 0;
+  let mass = n;
+  while ((mass = calculateFuelForMass(mass)) > 0) {
+    totalFuel += mass;
+  }
+
+  return totalFuel;
+};
+
+console.log(numbers.map(calculateMassIncludingFuel).reduce(sum));
